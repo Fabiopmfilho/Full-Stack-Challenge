7dc3e2b15f2666edda093d85d714047d5c748161
Fabio Filho <fabio.filho@acct.global>
2022-10-06 02:09:49 -0300

    styled front-end

--- web/src/pages/home/Index.jsx
+++ web/src/pages/home/Index.jsx
@@ -1,8 +1,8 @@
-function App() {
+export const Home = () => {
   return (
     <div className="h-screen bg-red-700 text-white p-4 flex flex-col items-center space-y-6">
       <header className="container max-w-5xl p-4 flex justify-center">
-        <img src="/images/logo.svg" className="w-40"/>
+        <img src="/images/logo-white.svg" className="w-40"/>
       </header>
 
       <div className="container max-w-5xl p-4 flex-1 flex flex-col items-center md:flex-row md:space-y-0 md:space-x-6">
@@ -19,17 +19,15 @@ function App() {
             Dê o seu palpite na Copa do Mundo do Catar 2022!
           </h1>
 
-          <button className="text-red-700 bg-white text-xl px-8 py-4 rounded-xl">
+          <a href="/signup" className="text-center text-red-700 bg-white text-xl px-8 py-4 rounded-xl">
             Criar minha conta
-          </button>
+          </a>
 
-          <button className="text-white border border-white text-xl px-8 py-4 rounded-xl">
-            Criar minha conta
-          </button>
+          <a href='/login' className="text-center text-white border border-white text-xl px-8 py-4 rounded-xl">
+            Fazer Login
+          </a>
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
